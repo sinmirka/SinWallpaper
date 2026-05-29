9d1032442bd8a8c530ba94375fb99023b2f8f7fd
sinmirka <oskar.rayapu@mail.ru>
2026-05-30 02:08:40 +0500

    Add bootstrapper startup registration

--- bootstrapper.py
+++ bootstrapper.py
@@ -1,5 +1,25 @@
 from pathlib import Path
 import subprocess
+import ctypes
+import sys
+import time
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+def request_admin_privileges():
+    try:
+        result = ctypes.windll.shell32.ShellExecuteW(
+            None, "runas", sys.executable, " ".join(sys.argv), None, 1
+        )
+        return result > 32  # Success if result > 32
+    except Exception as e:
+        print("Error:", str(e))
+        return False
+
 
 ROOT = Path(__file__).resolve().parent
 MAIN_ROOT = ROOT / "backend" / 'main.py'
@@ -25,7 +45,7 @@ def add_bat_to_ts(): #ts is task scheduler, not "this shit" lol...
     command = f'schtasks /create /tn "SinPaper Startup" /tr "{SERVICE_ROOT}" /sc onlogon /f'
     print(f"[DEBUG] Command: {command}")
     try:
-        result = subprocess.run(command)
+        result = subprocess.run(command, shell=True)
         print(f"[DEBUG] Return code {result.returncode}")
     except Exception as e:
         print(f"[ERROR] {e}")
@@ -38,6 +58,20 @@ def install_startup():
         print(f"[ERROR] {e}")
 
 def main():
+    print(f"Executable: {sys.executable}")
+    print(f"Args: {sys.argv}")
+    if not is_admin():
+        print("[WARN] Admin priveleges are required for work!")
+        success = request_admin_privileges()
+        if success:
+            print(f"[OK] Requested admin priveleges. Relaunching...")
+        else:
+            print(f"[WARN] Admin privilege request was denied!")
+        time.sleep(2)
+        sys.exit()
+    else:
+        print("[OK] Running with admin privileges!")
+
     print("""SinPaper Bootstrapper
     1. Enable Startup
     2. Disable Startup
@@ -48,6 +82,8 @@ def main():
         match choice:
             case "1":
                 install_startup()
+            case _:
+                print("Invalid choice")
 
 
 if __name__ == "__main__":
