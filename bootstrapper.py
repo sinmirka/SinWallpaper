16fe44b8beddee45639a92dddce1372674f4c99c
sinmirka <oskar.rayapu@mail.ru>
2026-05-30 01:42:35 +0500

    Add bootstrapper cli and startup installation feature

--- bootstrapper.py
+++ bootstrapper.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import subprocess
 
 ROOT = Path(__file__).resolve().parent
 MAIN_ROOT = ROOT / "backend" / 'main.py'
@@ -16,7 +17,38 @@ cd /d {ROOT}
 def create_startup_bat():
     with open(SERVICE_ROOT, "w") as f:
         f.write(generate_bat_content())
-        f.close()
+        print("[OK] Startup bat created")
 
 def add_bat_to_ts(): #ts is task scheduler, not "this shit" lol...
+    if not SERVICE_ROOT.exists():
+        create_startup_bat()
     command = f'schtasks /create /tn "SinPaper Startup" /tr "{SERVICE_ROOT}" /sc onlogon /f'
+    print(f"[DEBUG] Command: {command}")
+    try:
+        result = subprocess.run(command)
+        print(f"[DEBUG] Return code {result.returncode}")
+    except Exception as e:
+        print(f"[ERROR] {e}")
+
+def install_startup():
+    try:
+        create_startup_bat()
+        add_bat_to_ts()
+    except Exception as e:
+        print(f"[ERROR] {e}")
+
+def main():
+    print("""SinPaper Bootstrapper
+    1. Enable Startup
+    2. Disable Startup
+    3. Exit""")
+    while True:
+        choice = input("\n> ".strip())
+
+        match choice:
+            case "1":
+                install_startup()
+
+
+if __name__ == "__main__":
+    main()
